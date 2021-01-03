75633ab443ab2ac0b48b0c69f5fc22d2a5e4b8b6
Kit La Touche <kit@transneptune.net>
2021-01-02 17:59:14 -0700

    Add logging stuffs

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
 from tyche import run
+
+print("===========================")
+print(f"Python Path: {sys.path}")
+print(f"CWD: {os.getcwd()}")
+print("Environ:")
+print(os.environ)
+print("===========================")
 
 run()
